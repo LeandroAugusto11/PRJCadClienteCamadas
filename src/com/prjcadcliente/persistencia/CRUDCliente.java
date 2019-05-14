427d437c41d47d992e93dce6cd131d3249bcec59
LeandroAugusto11 <leaugustooficial@gmail.com>
2019-05-14 17:23:22 -0300

    Criação do corpo do método atualizar do CRUD

--- src/com/prjcadcliente/persistencia/CRUDCliente.java
+++ src/com/prjcadcliente/persistencia/CRUDCliente.java
@@ -72,7 +72,47 @@ public class CRUDCliente {
 		
 	}
 	public String atualizar(Cliente cliente) {
-		return null;
+		String msg = "";
+		 
+		// Criação dos Objetos para conexao com o banco de dados
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+			
+			// Não se faz UPDATE sem WHERE
+			String consulta = "UPDATE tbcliente SET nome=?,email=?,telefone=?,idade=? WHERE id=?";
+			
+			pst = con.prepareStatement(consulta);
+			
+			pst.setString(1, cliente.getNome() );
+			pst.setString(2, cliente.getEmail());
+			pst.setString(3, cliente.getTelefone());
+			pst.setInt(4, cliente.getIdade());
+			pst.setInt(5, cliente.getId());
+			
+			int r = pst.executeUpdate();
+			
+			
+			if(r > 0)
+				msg = " Atualizado com Sucesso!";
+			else
+				msg = "Não foi Possível Atualizar!";
+			
+					
+		}
+		// catch mostra o erro
+		catch(SQLException ex) {
+			msg = "Erro ao tentar atualizar:"+ex.getMessage();
+			}
+		catch(Exception e) {
+			msg = "Erro Inesperado:"+e.getMessage();
+		}
+		finally {
+			try{con.close();}catch(Exception e) {e.printStackTrace();}
+		}
+		
+		return msg;
+	
 	}
 	
 	public String deletar(Cliente cliente) {
